33d1039fe1556579660eedef8c1f9458e3de615d
Alexander Brovkin <brovkinas@gmail.com>
2026-01-21 18:24:14 +0300

    Add main_page fixture providing MainPage instance per test

--- conftest.py
+++ conftest.py
@@ -1,6 +1,13 @@
 import pytest
 
+from pages.main_page import MainPage
+
 
 @pytest.fixture(scope='session')
 def base_url(request):
     return request.config.getoption('--base-url')
+
+
+@pytest.fixture(scope='function')
+def main_page(page, base_url):
+    return MainPage(page, base_url)
