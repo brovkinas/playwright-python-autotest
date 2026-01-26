d381992a382897ab45b92ba94ebd5f02021b34c9
Alexander Brovkin <brovkinas@gmail.com>
2026-01-26 15:36:22 +0300

    add login_page fixture

--- conftest.py
+++ conftest.py
@@ -1,13 +1,19 @@
 import pytest
 
+from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def base_url(request):
-    return request.config.getoption('--base-url')
+    return request.config.getoption("--base-url")
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope="function")
 def main_page(page, base_url):
     return MainPage(page, base_url)
+
+
+@pytest.fixture(scope="function")
+def login_page(page, base_url):
+    return LoginPage(page, base_url)
