59970f6f7ed0e8fb42bde8b02ae68be9e7f47806
Alexander Brovkin <brovkinas@gmail.com>
2026-01-26 15:35:36 +0300

    introduce LoginPage object

---
+++ pages/login_page.py
@@ -0,0 +1,44 @@
+import allure
+
+from playwright.sync_api import expect
+from pages.base_page import BasePage
+
+
+class LoginPage(BasePage):
+
+    URL = '/login'
+    USERNAME_INPUT = '#username'
+    PASSWORD_INPUT = '#password'
+    LOGIN_BUTTON = 'button[type="submit"]'
+    FLASH_MESSAGE = '#flash'
+    SUCCESS_LOGIN_TEXT = 'You logged into a secure area!'
+    INVALID_USERNAME_TEXT = 'Your username is invalid!'
+    INVALID_PASSWORD_TEXT = 'Your password is invalid!'
+
+    @allure.step('Open login page')
+    def open(self):
+        super().open(self.URL)
+
+    @allure.step('Login with username: {username} and password: {password}')
+    def login(self, username: str, password: str):
+        self.page.fill(self.USERNAME_INPUT, username)
+        self.page.fill(self.PASSWORD_INPUT, password)
+        self.page.click(self.LOGIN_BUTTON)
+
+    @allure.step('Verify successful login')
+    def should_be_logged_in(self):
+        expect(self.page.locator(self.FLASH_MESSAGE)).to_contain_text(
+            self.SUCCESS_LOGIN_TEXT
+        )
+
+    @allure.step('Verify invalid username')
+    def should_have_invalid_username_error(self):
+        expect(self.page.locator(self.FLASH_MESSAGE)).to_contain_text(
+            self.INVALID_USERNAME_TEXT
+        )
+
+    @allure.step('Verify invalid password')
+    def should_have_invalid_password_error(self):
+        expect(self.page.locator(self.FLASH_MESSAGE)).to_contain_text(
+            self.INVALID_PASSWORD_TEXT
+        )
