e9ef50527c078099f688beb85a9ed5a7d1254392
Alexander Brovkin <brovkinas@gmail.com>
2026-01-28 16:13:33 +0300

    remove context / add pytest_plugins / add teardown for session_logger()

--- conftest.py
+++ conftest.py
@@ -1,11 +1,11 @@
-import os
 import pytest
 import logging
 
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 from core.logger import setup_logger
-from core.context import TestContext
+
+pytest_plugins = ["pytest_plugins.allure_hooks"]
 
 
 @pytest.fixture(scope="session")
@@ -28,29 +28,18 @@ def session_logger():
     logger = setup_logger()
     logger.info("=== LOGGER INITIALIZED ===")
 
+    yield
+
+    logging.shutdown()
+
 
 @pytest.fixture(scope="function", autouse=True)
 def test_context(request):
-    logger = logging.getLogger(__name__)
-
+    logger = logging.getLogger("autotests")
     nodeid = request.node.nodeid.replace("::", "_").replace("/", "_").replace(".", "_")
-
-    artifacts_dir = os.path.join("test-results", nodeid)
-    os.makedirs(artifacts_dir, exist_ok=True)
-
-    ctx = TestContext(nodeid=nodeid, artifacts_dir=artifacts_dir)
 
     logger.info(f"Start test: {nodeid}")
 
-    yield ctx
+    yield
 
     logger.info(f"Finish test: {nodeid}")
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-
-    outcome = yield
-    rep = outcome.get_result()
-
-    setattr(item, f"rep_{rep.when}", rep)
