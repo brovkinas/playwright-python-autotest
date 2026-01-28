766cfa844b4e9d10e39bc10ec61583099f836189
Alexander Brovkin <brovkinas@gmail.com>
2026-01-28 16:37:10 +0300

    change wait_until= / add _fill(), _click() for wait for element

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,19 +2,25 @@ import allure
 from playwright.sync_api import Page, expect
 
 
-class BasePage():
+class BasePage:
     def __init__(self, page: Page, base_url: str):
         self.page = page
         self.base_url = base_url
 
     @allure.step('Open url: {url}')
     def open(self, url: str = ''):
-        self.page.goto(
-            f'{self.base_url}{url}',
-            wait_until='domcontentloaded',
-            timeout=60000
-        )
+        self.page.goto(f'{self.base_url}{url}', wait_until='networkidle', timeout=60000)
 
     @allure.step('Check page title contains: {expected_title}')
     def should_have_title(self, expected_title: str):
         expect(self.page).to_have_title(expected_title)
+
+    def _fill(self, locator: str, value: str):
+        element = self.page.locator(locator)
+        expect(element).to_be_visible()
+        element.fill(value)
+
+    def _click(self, locator: str):
+        element = self.page.locator(locator)
+        expect(element).to_be_enabled()
+        element.click()
