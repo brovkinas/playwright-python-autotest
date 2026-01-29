22464f39bacbddcf0945019b892f7183fc54ce3b
Alexander Brovkin <brovkinas@gmail.com>
2026-01-29 17:21:57 +0300

    modify password value to invalid as expected for test_invalid_password_login

--- tests/test_login.py
+++ tests/test_login.py
@@ -33,7 +33,7 @@ def test_invalid_username_login(login_page):
 def test_invalid_password_login(login_page):
     try:
         login_page.open()
-        login_page.login('tomsmith', 'SuperSecretPassword!')
+        login_page.login('tomsmith', 'SuperSecretPassword!1')
         login_page.should_have_invalid_password_error()
     except Exception:
         login_page.attach_page_source()
