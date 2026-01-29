e262fc9036a1a482287c24b6a1bbd09de85f28da
Alexander Brovkin <brovkinas@gmail.com>
2026-01-29 17:16:24 +0300

    add try-except to attach page source in fail case

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,28 +1,40 @@
-import allure
+import allure  # noqa
 
 
 @allure.epic("Authentication")
 @allure.feature("Login by username and password")
 @allure.story("Successful Login by username and password")
 def test_success_login(login_page):
-    login_page.open()
-    login_page.login('tomsmith', 'SuperSecretPassword!')
-    login_page.should_be_logged_in()
+    try:
+        login_page.open()
+        login_page.login('tomsmith', 'SuperSecretPassword!')
+        login_page.should_be_logged_in()
+    except Exception:
+        login_page.attach_page_source()
+        raise
 
 
 @allure.epic("Authentication")
 @allure.feature("Login by username and password")
 @allure.story("Username Login error")
 def test_invalid_username_login(login_page):
-    login_page.open()
-    login_page.login('tomsmithh', 'SuperSecretPassword!')
-    login_page.should_have_invalid_username_error()
+    try:
+        login_page.open()
+        login_page.login('tomsmithh', 'SuperSecretPassword!')
+        login_page.should_have_invalid_username_error()
+    except Exception:
+        login_page.attach_page_source()
+        raise
 
 
 @allure.epic("Authentication")
 @allure.feature("Login by username and password")
 @allure.story("Password Login error")
 def test_invalid_password_login(login_page):
-    login_page.open()
-    login_page.login('tomsmith', 'SuperSecretPassword!!')
-    login_page.should_have_invalid_password_error()
+    try:
+        login_page.open()
+        login_page.login('tomsmith', 'SuperSecretPassword!')
+        login_page.should_have_invalid_password_error()
+    except Exception:
+        login_page.attach_page_source()
+        raise
