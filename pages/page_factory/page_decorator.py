52c7cb1cded38d08450ce1c1c9249f586d987786
Alexander Brovkin <brovkinas@gmail.com>
2026-02-09 17:03:18 +0300

    add page registration using decorator

---
+++ pages/page_factory/page_decorator.py
@@ -0,0 +1,38 @@
+# @page_decorator.py
+import logging
+from typing import TypeVar, Type
+
+from pages.base_page import BasePage
+from pages.page_factory.page_type import PageType
+from pages.page_factory.registry import PAGE_REGISTRY
+
+T = TypeVar("T", bound=BasePage)
+logger = logging.getLogger("autotests")
+
+
+def register_page(page_type: PageType):
+
+    def decorator(cls: Type[T]) -> Type[T]:
+
+        if not issubclass(cls, BasePage):
+            raise TypeError(f"{cls.__name__} must inherit from BasePage")
+
+        registered_cls = PAGE_REGISTRY.get(page_type)
+
+        if registered_cls is not None and registered_cls is not cls:
+            raise ValueError(
+                f"{page_type} already registered by {registered_cls.__name__}"
+            )
+
+        PAGE_REGISTRY[page_type] = cls
+
+        cls.PAGE_TYPE = page_type
+
+        if cls.PAGE_TYPE is None:
+            raise RuntimeError("PAGE_TYPE was not assigned")
+
+        logger.debug(f"Registered page {cls.__name__} -> {page_type.name}")
+
+        return cls
+
+    return decorator
