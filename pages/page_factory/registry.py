fb72fa325322450d7edd19d210a33ddee0aa94ff
Alexander Brovkin <brovkinas@gmail.com>
2026-02-09 17:10:43 +0300

    refactor PAGE_REGISTRY / add typing

--- pages/page_factory/registry.py
+++ pages/page_factory/registry.py
@@ -1,8 +1,5 @@
-from pages.login_page import LoginPage
-from pages.main_page import MainPage
+from typing import Type
+from pages.base_page import BasePage
 from pages.page_factory.page_type import PageType
 
-PAGE_REGISTRY = {
-    PageType.LOGIN: LoginPage,
-    PageType.MAIN: MainPage,
-}
+PAGE_REGISTRY: dict[PageType, Type[BasePage]] = {}
