a65d553191db5cf2648bc8fbba3facba7ccf112c
Alexander Brovkin <brovkinas@gmail.com>
2026-02-09 17:02:21 +0300

    add PageType <-> PageClass mapping / add urljoin for base_url/url

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,14 @@
+from urllib.parse import urljoin
+
 import allure  # noqa
 from playwright.sync_api import Page, expect
 
+from pages.page_factory.page_type import PageType
+
 
 class BasePage:
+    PAGE_TYPE: PageType | None = None
+
     def __init__(self, page: Page, base_url: str):
         self.page = page
         self.base_url = base_url
@@ -17,12 +23,12 @@ class BasePage:
         expect(element).to_be_enabled()
         element.click()
 
-    @allure.step('Open url: {url}')
-    def open(self, url: str = ''):
+    @allure.step("Open url: {url}")
+    def open(self, url: str = ""):
         self.page.goto(
-            f'{self.base_url}{url}', wait_until='domcontentloaded', timeout=30000
+            urljoin(self.base_url, url), wait_until="domcontentloaded", timeout=30000
         )
 
-    @allure.step('Check page title contains: {expected_title}')
+    @allure.step("Check page title contains: {expected_title}")
     def should_have_title(self, expected_title: str):
         expect(self.page).to_have_title(expected_title)
