f45457280ffb42788853571487d3076c0ec8858a
Alexander Brovkin <brovkinas@gmail.com>
2026-02-09 17:06:44 +0300

    refactor create method

--- pages/page_factory/factory.py
+++ pages/page_factory/factory.py
@@ -1,23 +1,22 @@
 from playwright.sync_api import Page
 
+from pages.base_page import BasePage
 from pages.page_factory.registry import PAGE_REGISTRY
 from pages.page_factory.page_type import PageType
 
 
 class PagesFactory:
-    """
-    Мини-фабрика страниц.
-    Создаёт и возвращает экземпляр класса страницы,
-    соответствующий переданному PageType, используя PAGE_REGISTRY.
-    """
 
     def __init__(self, page: Page, base_url: str):
+
         self.page = page
         self.base_url = base_url
 
-    def create(self, page_type: PageType):
-        try:
-            page_class = PAGE_REGISTRY[page_type]
-            return page_class(self.page, self.base_url)
-        except KeyError:
-            raise ValueError(f'"{page_type}" не зарегистрирован в PAGE_REGISTRY')
+    def create(self, page_type: PageType) -> BasePage:
+
+        page_cls = PAGE_REGISTRY.get(page_type)
+
+        if page_cls is None:
+            raise ValueError(f"PageType {page_type.name} is not registered")
+
+        return page_cls(self.page, self.base_url)
