fbf400bc32659ad00178fd9f1b536e3ce219ebe8
Alexander Brovkin <brovkinas@gmail.com>
2026-02-10 19:30:23 +0300

    change allure descriptions and test data

--- tests/test_login.py
+++ tests/test_login.py
@@ -5,29 +5,29 @@ from pages.page_factory.page_type import PageType
 
 @allure.epic("Authentication")
 @allure.feature("Login by username and password")
-@allure.story("Successful Login by username and password")
+@allure.story("Successful Login")
 def test_success_login(pages):
     login_page = pages.create(PageType.LOGIN)
     login_page.open()
-    login_page.login('tomsmith', 'SuperSecretPassword!')
+    login_page.login("tomsmith", "SuperSecretPassword!")
     login_page.should_be_logged_in()
 
 
 @allure.epic("Authentication")
 @allure.feature("Login by username and password")
-@allure.story("Username Login error")
+@allure.story("Login error: username")
 def test_invalid_username_login(pages):
     login_page = pages.create(PageType.LOGIN)
     login_page.open()
-    login_page.login('wrong-username', 'SuperSecretPassword!')
+    login_page.login("invalid-username", "SuperSecretPassword!")
     login_page.should_have_invalid_username_error()
 
 
 @allure.epic("Authentication")
 @allure.feature("Login by username and password")
-@allure.story("Password Login error")
+@allure.story("Login error: password")
 def test_invalid_password_login(pages):
     login_page = pages.create(PageType.LOGIN)
     login_page.open()
-    login_page.login('tomsmith', 'invalid_SuperSecretPassword!')
+    login_page.login("tomsmith", "invalid-password")
     login_page.should_have_invalid_password_error()
