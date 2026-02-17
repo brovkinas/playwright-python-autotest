1097d15dfa596672047508a9a6619c37c9ab50ac
Alexander Brovkin <brovkinas@gmail.com>
2026-02-17 21:31:59 +0300

    refactor/add new tests with .env creds approach

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,15 +1,27 @@
 import allure  # noqa
 
 from core.page_factory.page_type import PageType
+from core.enums.user_roles import UserRole
+from test_data.roles import get_user
 
 
 @allure.epic("Authentication")
 @allure.feature("Login by username and password")
-@allure.story("Successful Login")
-def test_success_login(pages):
+@allure.story("Successful Login by Admin")
+def test_success_login_admin(pages):
     login_page = pages.create(PageType.LOGIN)
     login_page.open()
-    login_page.login("tomsmith", "SuperSecretPassword!")
+    login_page.user_login(get_user(UserRole.ADMIN))
+    login_page.should_be_logged_in()
+
+
+@allure.epic("Authentication")
+@allure.feature("Login by username and password")
+@allure.story("Successful Login by common User")
+def test_success_login_user(pages):
+    login_page = pages.create(PageType.LOGIN)
+    login_page.open()
+    login_page.user_login(get_user(UserRole.USER))
     login_page.should_be_logged_in()
 
 
@@ -19,7 +31,7 @@ def test_success_login(pages):
 def test_invalid_username_login(pages):
     login_page = pages.create(PageType.LOGIN)
     login_page.open()
-    login_page.login("invalid-username", "SuperSecretPassword!")
+    login_page.user_login(get_user(UserRole.USER_WRONG_NAME))
     login_page.should_have_invalid_username_error()
 
 
@@ -29,5 +41,5 @@ def test_invalid_username_login(pages):
 def test_invalid_password_login(pages):
     login_page = pages.create(PageType.LOGIN)
     login_page.open()
-    login_page.login("tomsmith", "invalid-password")
+    login_page.user_login(get_user(UserRole.USER_WRONG_PASSWORD))
     login_page.should_have_invalid_password_error()
